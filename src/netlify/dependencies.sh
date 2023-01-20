986b3bdb816d137d9532e21a55df6e890c3c3708
Lukap22 <lukstarwow@gmail.com>
2023-01-20 12:30:20 +0100

    something

--- src/netlify/dependencies.sh
+++ src/netlify/dependencies.sh
@@ -8,14 +8,12 @@ ensure_npm () {
 if command -v npm &> /dev/null; then
   echo "npm is already installed"
 else
-  echo "installing npm"
-  # install npm
-  apt-get install -y npm
+ echo "installing npm"
+  # install npm using curl
+  curl -L https://www.npmjs.com/install.sh | sh
 fi
 }
 
 ensure_npm
-
-
 
 npm install netlify-cli -g
