68d441ab6bc3c2c65d201635894fdaa5e4b162eb
Lukap22 <lukstarwow@gmail.com>
2023-01-20 12:07:00 +0100

    something

--- src/netlify/dependencies.sh
+++ src/netlify/dependencies.sh
@@ -4,34 +4,18 @@
 
 set -e
 
-ensure_curl () {
-    if ! type curl >/dev/null 2>&1; then
-        apt-get update -y && apt-get -y install --no-install-recommends curl
-    fi 
+ensure_npm () {
+if command -v npm &> /dev/null; then
+  echo "npm is already installed"
+else
+  echo "installing npm"
+  # install npm
+  apt-get install -y npm
+fi
 }
 
+ensure_npm
 
 
-ensure_featmake () {
-    if ! type featmake > /dev/null 2>&1; then
-        temp_dir=/tmp/featmake-download
-        mkdir -p $temp_dir
 
-        curl -sSL -o $temp_dir/featmake https://github.com/devcontainers-contrib/cli/releases/download/v0.0.9/featmake 
-        curl -sSL -o $temp_dir/checksums.txt https://github.com/devcontainers-contrib/cli/releases/download/v0.0.9/checksums.txt
-
-        (cd $temp_dir ; sha256sum --check --strict $temp_dir/checksums.txt)
-
-        chmod a+x $temp_dir/featmake
-        mv -f $temp_dir/featmake /usr/local/bin/featmake
-
-        rm -rf $temp_dir
-    fi
-}
-
-ensure_curl
-
-ensure_featmake
-
-# installing ghcr.io/devcontainers-contrib/features/npm-package:1.0.1
-PACKAGE="netlify-cli" VERSION="$VERSION" featmake "ghcr.io/devcontainers-contrib/features/npm-package:1.0.1"
+npm install netlify-cli -g
